4145a753e3270e856475d7f8abb4d75e7c12922f
Turbin Viktor <turbinvr1@gmail.com>
2023-03-25 02:10:27 +0300

    first commit

---
+++ 123421.py
@@ -0,0 +1,61 @@
+import random
+from typing import Tuple
+
+
+def miller_rabin(n, k=40):
+
+    # Implementation uses the Miller-Rabin Primality Test
+    # The optimal number of rounds for this test is 40
+    # See http://stackoverflow.com/questions/6325576/how-many-iterations-of-rabin-miller-should-i-use-for-cryptographic-safe-primes
+    # for justification
+
+    # If number is even, it's a composite number
+
+    if n == 2:
+        return True
+
+    if n % 2 == 0:
+        return False
+
+    r, s = 0, n - 1
+    while s % 2 == 0:
+        r += 1
+        s //= 2
+    for _ in range(k):
+        a = random.randrange(2, n - 1)
+        x = pow(a, s, n)
+        if x == 1 or x == n - 1:
+            continue
+        for _ in range(r - 1):
+            x = pow(x, 2, n)
+            if x == n - 1:
+                break
+        else:
+            return False
+    return True
+
+
+def extend_gcd(a: int, b: int) -> Tuple[int, int, int]:
+    """return (g, x, y) such that a*x + b*y = g = gcd(a, b)"""
+    x0, x1, y0, y1 = 0, 1, 1, 0
+    while a != 0:
+        (q, a), b = divmod(b, a), a
+        y0, y1 = y1, y0 - q * y1
+        x0, x1 = x1, x0 - q * x1
+    return b, x0, y0
+
+
+def fast_exp(b, e, m):
+    r = 1
+    if 1 & e:
+        r = b
+    while e:
+        e >>= 1
+        b = (b * b) % m
+        if e & 1:
+            r = (r * b) % m
+    return r
+
+
+print(pow(2**512, 2**10, 2**50))
+print(fast_exp(2**512, 2**10, 2**50))
